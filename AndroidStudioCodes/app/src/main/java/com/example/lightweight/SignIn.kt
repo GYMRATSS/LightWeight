816471fa699c972ce70c27609da41c5f916e8776
Tuğçe Yavuz <100586556+tugceyavuz@users.noreply.github.com>
2022-10-25 22:31:15 +0300

    new action page added

--- AndroidStudioCodes/app/src/main/java/com/example/lightweight/SignIn.kt
+++ AndroidStudioCodes/app/src/main/java/com/example/lightweight/SignIn.kt
@@ -4,19 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity() {
+class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in)
 
         val actionBar = supportActionBar
         actionBar!!.hide()
 
-        val signIn: Button = findViewById (R.id.signIn)
+        val turnBtn: ImageButton = findViewById(R.id.turnHome)
 
-        signIn.setOnClickListener() {
-            val intent = Intent(this, SignIn::class.java)
+        turnBtn.setOnClickListener() {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
