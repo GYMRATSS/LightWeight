a7bdc3fb6503741fddc182abb9ebe06458c436ca
edtheheaddie <edanurcanakci1@gmail.com>
2022-12-04 19:21:55 +0300

    progress bar stuff

--- AndroidStudioCodes/app/src/main/java/com/example/lightweight/Profile.kt
+++ AndroidStudioCodes/app/src/main/java/com/example/lightweight/Profile.kt
@@ -164,14 +164,14 @@ class Profile : AppCompatActivity() {
     }
 
     fun calculate(){
-        var startWeightText: TextView = findViewById (R.id.start)
-        var initialWeight = startWeightText?.text.toString().toIntOrNull() ?: 0
+        val startWeightText: TextView = findViewById (R.id.start)
+        val initialWeight = startWeightText?.text.toString().toIntOrNull() ?: 0
 
-        var weightGoalText: TextView = findViewById (R.id.goal)
-        var weightGoal = weightGoalText?.text.toString().toIntOrNull() ?: 0
+        val weightGoalText: TextView = findViewById (R.id.goal)
+        val weightGoal = weightGoalText?.text.toString().toIntOrNull() ?: 0
 
-        var currentWeightText: TextView = findViewById (R.id.weight)
-        var currentWeight = currentWeightText?.text.toString().toIntOrNull() ?: 0
+        val currentWeightText: TextView = findViewById (R.id.weight)
+        val currentWeight = currentWeightText?.text.toString().toIntOrNull() ?: 0
 
         var weightProgress = findViewById<ProgressBar>(R.id.weightProgressBar)
 
