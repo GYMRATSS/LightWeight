1e17daf47c36b70f1e18912f9a22fdbcaff5f056
Umut Can <umutcanozay@hotmail.com>
2022-12-08 22:54:09 +0300

    yemek sayfasında yenen yemeklerin yazmayan isimleri artık yazıyor (id de tutuluyor database'de)

--- AndroidStudioCodes/app/src/main/java/com/example/lightweight/EnterFood.kt
+++ AndroidStudioCodes/app/src/main/java/com/example/lightweight/EnterFood.kt
@@ -108,7 +108,7 @@ class EnterFood : AppCompatActivity(), AdapterClass.ClickListener{
 
         userReference?.child("besin")?.child("besin kayıtları")?.child(LocalDate.now().toString())?.child(Meal.id.toString())?.setValue(Meal)
         Thread.sleep(50)
-        userReference?.child("besin")?.child("besin kayıtları")?.child(LocalDate.now().toString())?.child(Meal.id.toString())?.child("id")?.removeValue()
+        //userReference?.child("besin")?.child("besin kayıtları")?.child(LocalDate.now().toString())?.child(Meal.id.toString())?.child("id")?.removeValue()
         Thread.sleep(50)
 
         finish()
