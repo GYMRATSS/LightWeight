20feb539d1eb89de3b2560e949b04a9cc1e9222d
Tuğçe Yavuz <100586556+tugceyavuz@users.noreply.github.com>
2023-01-07 20:35:43 +0300

    changes

--- AndroidStudioCodes/app/src/main/java/com/example/lightweight/CustomProgram.kt
+++ AndroidStudioCodes/app/src/main/java/com/example/lightweight/CustomProgram.kt
@@ -1,17 +1,19 @@
 package com.example.lightweight
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.SearchView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
-import java.time.LocalDate
+import android.os.Bundle
 
-class CustomProgram {
+class CustomProgram : AppCompatActivity() , AdaptoWList.ClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_program2)
+    }
 
+    override fun onStart() {
+        super.onStart()
+    }
 
+    override fun ClickedItem(workoutPlanList: workoutPlanList) {
+        TODO("Not yet implemented")
+    }
 }
