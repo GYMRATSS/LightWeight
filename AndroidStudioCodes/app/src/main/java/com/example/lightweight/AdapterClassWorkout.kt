4f51ba27472eabcfc993ebd5badea9c9bbae9752
Ilayda Arar <ilaydaarar@outlook.com>
2023-01-08 16:27:14 +0300

    workout displayi duzeltildi

--- AndroidStudioCodes/app/src/main/java/com/example/lightweight/AdapterClassWorkout.kt
+++ AndroidStudioCodes/app/src/main/java/com/example/lightweight/AdapterClassWorkout.kt
@@ -9,15 +9,15 @@ import android.widget.TextView
 
 class AdapterClassWorkout(var programlist: ArrayList<workoutplan>) : RecyclerView.Adapter<MyViewHolderWorkout>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolderWorkout {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.holder_program, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.holder_customprogram, parent, false)
         return MyViewHolderWorkout(view)
     }
 
-    override fun onBindViewHolder(holder_program: MyViewHolderWorkout, position: Int) {
+    override fun onBindViewHolder(holder_customprogram: MyViewHolderWorkout, position: Int) {
         val Modal = programlist[position]
-        holder_program.planid_.text = programlist[position].id
+        holder_customprogram.planid_.text = programlist[position].id
         Size_programlist.mySizePL = programlist.size
-        holder_program.descr.text = "Hareket: " +programlist[position].id + "\n" +  "Ağırlık: " + programlist[position].ağırlık +"\n"+
+        holder_customprogram.descr.text = "Hareket: " +programlist[position].id + "\n" +  "Ağırlık: " + programlist[position].ağırlık +"\n"+
                 "Set: " +programlist[position].set + "\n" + "Tekrar: " +programlist[position].tekrar + "\n"
     }
 
@@ -30,8 +30,8 @@ class AdapterClassWorkout(var programlist: ArrayList<workoutplan>) : RecyclerVie
         var descr: TextView
 
         init {
-            planid_ = itemView.findViewById(R.id.programId)
-            descr = itemView.findViewById(R.id.description)
+            planid_ = itemView.findViewById(R.id.customprogramId)
+            descr = itemView.findViewById(R.id.customdescription)
         }
 
 
